b58f0c0ded98d72096f3575659ed7e2602c858f6
unconst <jake@bittensor.com>
2025-08-15 15:00:00 -0500

    remove crap

--- experiments/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-GRAIL Security Experiments
-
-This module contains rigorous experiments to validate GRAIL's security properties,
-particularly focusing on the dot product uniqueness problem and multi-constraint defense.
-"""
